d045429fe6fba82765ff704ea6ffefc36e1be80d
Teighan Miller <teighanmiller@Teighans-Laptop.local>
2026-05-13 17:21:22 -0400

    adding history to chat to remove error and prepare for future implementations of history

--- src/chat.py
+++ src/chat.py
@@ -2,7 +2,7 @@ import gradio as gr
 from backend import process_message
 
 
-def chat(user_message: str) -> str:
+def chat(user_message: str, history: list) -> str:
     return process_message(user_message)
 
 
